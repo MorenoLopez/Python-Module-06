ec5de7f6b6e158ce9123dc47e2e8efc2307ec9c4
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-08-08 04:47:35 +0000

    elements.py

---
+++ elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   elements.py                                          :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/07/24 08:37:05 by horarivo            #+#    #+#            #
+#   Updated: 2026/07/24 08:37:07 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+def create_fire() -> str:
+    return "Fire element created"
+
+
+def create_water() -> str:
+    return "Water element created"
