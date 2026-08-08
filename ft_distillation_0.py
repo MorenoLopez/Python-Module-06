6ba59825ffd820ae0f9d04b7f8e960026c78cbd1
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-08-08 04:48:27 +0000

    ft_distillation_

---
+++ ft_distillation_0.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_distillation_0.py                                 :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/07/24 08:37:39 by horarivo            #+#    #+#            #
+#   Updated: 2026/07/24 08:37:41 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+from alchemy.potions import healing_potion, strength_potion
+
+print("=== Distillation 0 ===")
+print("Direct access to alchemy/potions.py")
+print(f"Testing strength_potion: {strength_potion()}")
+print(f"Testing healing_potion: {healing_potion()}")
---
+++ ft_distillation_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_distillation_1.py                                 :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/07/24 08:37:43 by horarivo            #+#    #+#            #
+#   Updated: 2026/07/24 12:22:08 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+import alchemy
+
+print("=== Distillation 1 ===")
+print("Using: 'import alchemy' structure to access potions")
+print(f"Testing strength_potion: {alchemy.strength_potion()}")
+print(f"Testing heal alias: {alchemy.heal()}")
